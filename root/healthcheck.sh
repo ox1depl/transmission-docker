1b97353db2563f7e94aa2b435e3375406ddfdf1d
ox1depl <kasperski.bartlomiej@gmail.com>
2020-04-05 13:27:27 +0200

    Add healthcheck based on rpc api

--- root/healthcheck.sh
+++ root/healthcheck.sh
@@ -3,4 +3,6 @@
 TARGET=localhost
 CURL_OPTS="--connect-timeout 15 --silent --show-error --fail"
 
-curl ${CURL_OPTS} "http://${TARGET}:9091" >/dev/null
+SESSION_HEADER=$(curl --connect-timeout 15 --silent --anyauth --user admin:Rowerek~~ http://${TARGET}:9091/transmission/rpc | sed 's/.*<code>//g;s/<\/code>.*//g')
+
+curl ${CURL_OPTS} --anyauth --user ${USER}:${PASS} --header ${SESSION_HEADER} http://${TARGET}:9091/transmission/rpc -d "{\"method\":\"session-stats\"}" >/dev/null
